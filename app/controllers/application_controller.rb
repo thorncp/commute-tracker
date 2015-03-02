c7622cc2f9c026866a66988e6e82e95a6a68762a
Chris Thorn <chris@thorn.co>
2015-03-01 17:02:20 -0800

    Authenticate all actions
    Uses HTTP token authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    unless check_token
+      render json: { error: "Unauthorized" }, status: 401
+    end
+  end
+
+  def check_token
+    authenticate_with_http_token do |token, _options|
+      token == ENV["AUTH_TOKEN"]
+    end
+  end
 end
--- spec/requests/hello_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "hello world" do
-  it "is alive" do
-    get hello_path
-
-    expect(JSON.parse(response.body)["message"]).to eq("Hello, World!")
-  end
-end
