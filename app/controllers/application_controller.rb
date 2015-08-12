6046035012bd6a3313ed7cc565b361e571b59f39
Chris Thorn <chris@thorn.co>
2015-08-12 10:38:51 -0700

    Use current user time zone
    * Sets the timezone to the current user's zone for the duration of their
    request.
    * Removes javascript to display the commute day in the user's time zone.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
 
+  around_action :use_current_user_time_zone, if: :signed_in?
   before_action :require_login
+
+  private
+
+  def use_current_user_time_zone
+    Time.use_zone(current_user.timezone) do
+      yield
+    end
+  end
 end
